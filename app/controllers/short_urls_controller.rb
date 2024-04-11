8b9c2671c9a9f2a4d784d9b843bb14dea1e343fb
Benjamin <Benjaminkakai001@gmail.com>
2024-04-11 10:26:54 +0300

    yup that's me, you probably are wondring how I ended up here

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -1,13 +1,22 @@
-# app/controllers/short_urls_controller.rb
-
 class ShortUrlsController < ApplicationController
-    def redirect
-      short_url = ShortUrl.find_by(short_code: params[:short_code])
-      if short_url
-        redirect_to short_url.original_url
-      else
-        render plain: "Shortened URL not found", status: :not_found
-      end
+  # Method to shorten a URL
+  def shorten
+    long_url = params[:url] # Change from :long_url to :url
+    short_url = generate_short_url
+
+    # Assuming you have a model named ShortUrl
+    short_url_record = ShortUrl.new(original_url: long_url, short_code: short_url)
+
+    if short_url_record.save
+      render json: { short_url: short_url_record.short_code }, status: :created
+    else
+      render json: { error: 'Failed to create short URL' }, status: :unprocessable_entity
     end
   end
-  
+
+  private
+
+  def generate_short_url
+    SecureRandom.uuid[0..5]
+  end
+end
