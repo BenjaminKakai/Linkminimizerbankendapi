04fd64c97389edf08f3a549fc142fc1f401ead95
Benjamin <Benjaminkakai001@gmail.com>
2024-04-11 12:37:53 +0300

    y

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -1,18 +1,19 @@
 class ShortUrlsController < ApplicationController
   # Method to shorten a URL
-  def shorten
-    long_url = params[:url] # Change from :long_url to :url
-    short_url = generate_short_url
 
-    # Assuming you have a model named ShortUrl
-    short_url_record = ShortUrl.new(original_url: long_url, short_code: short_url)
+  # Method to shorten a URL
+def shorten
+  long_url = params[:url]
 
-    if short_url_record.save
-      render json: { short_url: short_url_record.short_code }, status: :created
-    else
-      render json: { error: 'Failed to create short URL' }, status: :unprocessable_entity
-    end
+  short_url_record = ShortUrl.new(original_url: long_url, short_code: generate_short_url)
+
+  if short_url_record.save
+    render json: { short_url: short_url_record.short_code }, status: :created
+  else
+    render json: { error: short_url_record.errors.full_messages.join(', ') }, status: :unprocessable_entity
   end
+end
+
 
   # Method to display a list of short URLs
   def index
