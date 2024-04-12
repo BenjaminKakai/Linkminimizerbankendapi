38fb1c81538a320dda6678298326214d9ac875be
Benjamin <Benjaminkakai001@gmail.com>
2024-04-12 11:56:45 +0300

    Yup

--- app/controllers/cors_controller.rb
+++ app/controllers/cors_controller.rb
@@ -1,9 +1,10 @@
 # app/controllers/cors_controller.rb
+
 class CorsController < ApplicationController
     # Method to handle CORS preflight requests
     def cors_preflight_check
       # Set appropriate response headers for CORS
-      headers['Access-Control-Allow-Origin'] = '*' # Adjust this based on your frontend URL
+      headers['Access-Control-Allow-Origin'] = 'https://vercel.com/benjaminkakais-projects/link-minimizer-react-front-end/BeZiXLemnE78T2qYcZ917yp3q5Hk' # Replace with your Vercel frontend URL
       headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
       headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
       headers['Access-Control-Max-Age'] = '1728000'
