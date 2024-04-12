03b445110815bdec203bc9ddf8fbfa716dcca887
Benjamin <Benjaminkakai001@gmail.com>
2024-04-12 12:03:04 +0300

    Yup

--- app/controllers/cors_controller.rb
+++ app/controllers/cors_controller.rb
@@ -4,7 +4,7 @@ class CorsController < ApplicationController
     # Method to handle CORS preflight requests
     def cors_preflight_check
       # Set appropriate response headers for CORS
-      headers['Access-Control-Allow-Origin'] = 'https://vercel.com/benjaminkakais-projects/link-minimizer-react-front-end/BeZiXLemnE78T2qYcZ917yp3q5Hk' # Replace with your Vercel frontend URL
+      headers['Access-Control-Allow-Origin'] = 'https://link-minimizer-react-front-end.vercel.app' # Replace with your Vercel frontend URL
       headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
       headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
       headers['Access-Control-Max-Age'] = '1728000'
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://vercel.com/benjaminkakais-projects/link-minimizer-react-front-end/BeZiXLemnE78T2qYcZ917yp3q5Hk', 'http://localhost:3001'
+      origins 'https://link-minimizer-react-front-end.vercel.app', 'http://localhost:3001'
       resource '*', 
                headers: :any, 
                methods: [:get, :post, :put, :patch, :delete, :options, :head],
